404802adc78707a4b7fc8aca18115ddbe48f931d
aijahgarcia <aijah_garcia@brown.edu>
2025-05-08 13:43:52 -0400

    visualizer is done! thank god! i hate fighting with javascript

--- visualizer.js
+++ visualizer.js
@@ -15,14 +15,12 @@ function incrementState() {
     if (current_state < last_state) {
         current_state += 1;
     }
-    stage.render(svg);
 }
 
 function decrementState() {
     if (current_state != 0) {
         current_state -= 1;
     }
-    stage.render(svg);
 }
 
 var state_label = new TextBox({
@@ -44,6 +42,7 @@ var prev_button = new TextBox({
             event: "click",
             callback: function (ele, ev, d) {
                 decrementState();
+                render();
             },
         },
     ],
@@ -61,6 +60,7 @@ var prev_button_label = new TextBox({
             event: "click",
             callback: function (ele, ev, d) {
                 decrementState();
+                render();
             },
         },
     ],
@@ -77,6 +77,7 @@ var next_button = new TextBox({
             event: "click",
             callback: function (ele, ev, d) {
                 incrementState();
+                render();
             },
         },
     ],
@@ -94,6 +95,7 @@ var next_button_label = new TextBox({
             event: "click",
             callback: function (ele, ev, d) {
                 incrementState();
+                render();
             },
         },
     ],
@@ -119,35 +121,97 @@ function proclet_color(proclet_name) {
     }
 }
 
-const machines = Machine.atoms().map((ltup) => fam(ltup));
+function render() {
+    const machines = Machine.atoms().map((ltup) => fam(ltup));
+    var placed_proclets = [];
+    stage.elements = [];
 
-let machineWidth = 240;
-let machineHeight = 160;
-let procletWidth = 30;
-let procletHeight = 20;
-let spacing = 20;
+    let machineWidth = 240;
+    let machineHeight = 160;
+    let procletWidth = 30;
+    let procletHeight = 20;
+    let spacing = 20;
 
-machines.forEach((machine, i) => {
-    // Draw the machine rectangle
-    let machineRect = new Rectangle({
-        coords: { x: 50, y: 50 + i * (machineHeight + spacing) },
-        width: machineWidth,
-        height: machineHeight,
-        color: "lightgray",
-        borderColor: "black",
-        borderWidth: 2,
-        label: `Machine ${i}`
+    machines.forEach((machine, i) => {
+        // Draw the machine rectangle
+        let machineRect = new Rectangle({
+            coords: {
+                x: 50,
+                y: 70 + i * (machineHeight + spacing)
+            },
+            width: machineWidth,
+            height: machineHeight,
+            color: "lightgray",
+            borderColor: "black",
+            borderWidth: 2,
+            label: `Machine ${i}`
+        });
+        stage.add(machineRect);
+
+        // Draw the proclets inside the machine
+        var mach_name = machine.toString().replace("[", "");
+        var mach_instance = instances[current_state].atom(mach_name);
+        var mach_proclets = mach_instance.proclets.tuples().map((ltup) => fam(ltup));
+        var mach_proc_array = [];
+
+        mach_proclets.forEach((proclet, j) => {
+            var proclet_name = proclet.toString().replace("[", "");
+            placed_proclets.push(proclet_name);
+            let procletRect = new Rectangle({
+                coords: {
+                    x: 60 + (j % 3) * (procletWidth + 5),
+                    y: 80 + i * (machineHeight + spacing) + Math.floor(j / 3) * (procletHeight + 5)
+                },
+                width: procletWidth,
+                height: procletHeight,
+                color: () => proclet_color(proclet_name),
+                borderColor: "black",
+                borderWidth: 2,
+                label: proclet_name.substring(proclet_name.length - 1)
+            });
+            stage.add(procletRect);
+            mach_proc_array.push(proclet_name);
+        });
+
     });
-    stage.add(machineRect);
 
-    // Draw the proclets inside the machine
-    var proclets = machine.proclets.tuples().map((ltup) => fam(ltup));
-    proclets.forEach((proclet, j) => {
+    polypoints = [
+        { x: 75 + machineWidth, y: 30 },
+        { x: 75 + machineWidth, y: 450 }
+    ]
+
+    let line = new Line({
+        points: polypoints,
+        color: 'black',
+        width: 2,
+        arrow: false,
+        style: "dotted"
+    });
+    stage.add(line)
+
+
+    stage.add(new TextBox({
+        text: 'Machines & Scheduled Proclets:',
+        coords: { x: 170, y: 50 },
+        color: 'black',
+        fontSize: 16
+    }))
+
+    stage.add(new TextBox({
+        text: 'All Proclets:',
+        coords: { x: 140 + machineWidth, y: 50 },
+        color: 'black',
+        fontSize: 16
+    }))
+
+    // draw all compute proclets
+    const comp_proclets = Compute_Proclet.atoms().map((ltup) => fam(ltup));
+    comp_proclets.forEach((proclet, j) => {
         var proclet_name = proclet.toString().replace("[", "");
         let procletRect = new Rectangle({
             coords: {
-                x: 60 + (j % 3) * (procletWidth + 5),
-                y: 60 + i * (machineHeight + spacing) + Math.floor(j / 3) * (procletHeight + 5)
+                x: 100 + machineWidth + (j % 3) * (procletWidth + 5),
+                y: 70 + Math.floor(j / 3) * (procletHeight + 5)
             },
             width: procletWidth,
             height: procletHeight,
@@ -158,6 +222,29 @@ machines.forEach((machine, i) => {
         });
         stage.add(procletRect);
     });
-});
 
-stage.render(svg);
+
+    // draw all memory proclets
+    const mem_proclets = Memory_Proclet.atoms().map((ltup) => fam(ltup));
+    mem_proclets.forEach((proclet, j) => {
+        var proclet_name = proclet.toString().replace("[", "");
+        let procletRect = new Rectangle({
+            coords: {
+                x: 100 + machineWidth + (j % 3) * (procletWidth + 5),
+                y: 110 + Math.floor(j / 3) * (procletHeight + 5)
+            },
+            width: procletWidth,
+            height: procletHeight,
+            color: () => proclet_color(proclet_name),
+            borderColor: "black",
+            borderWidth: 2,
+            label: proclet_name.substring(proclet_name.length - 1)
+        });
+        stage.add(procletRect);
+    });
+
+    stage.render(svg);
+
+}
+
+render();
